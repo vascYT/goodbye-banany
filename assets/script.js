fddedd0994eb6c1b423591f06dcb4fa6459e1fc0
vasc <nicovanaschaufler@gmail.com>
2021-07-30 04:34:39 +0200

    Add YT video

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
-const ids = ["ys4rkcMbtjY", "KWtwIf-TSlo", "xfrdxxDjXuw", "LH5ay10RTGY"];
+const ids = [
+  "ys4rkcMbtjY",
+  "KWtwIf-TSlo",
+  "xfrdxxDjXuw",
+  "LH5ay10RTGY",
+  "PCj0zLwci8U",
+];
 
 const randomId = ids[Math.floor(Math.random() * ids.length)];
 document
